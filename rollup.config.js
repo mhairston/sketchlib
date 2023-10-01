c01c7a5957045a4c327813fd68cff3f9e0fb6660
Michael Hairston <typespace@hey.com>
2023-10-01 16:42:10 -0400

    rollup.config: Tweaks to config.
    * Add minification.
    * Output copy of minified file without version #.

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,27 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import { readFileSync } from "fs";
+const pkg = JSON.parse(readFileSync('package.json', {encoding: 'utf8'}));
 
 export default {
   input: 'src/main.js',
-  output: {
-    file: 'dist/sketchlib.js',
-    format: 'esm'
-  },
+  output: [
+    {
+      file: `dist/sketchlib-${pkg.version}.js`,
+      format: 'esm'
+    },
+    {
+      file: `dist/sketchlib-${pkg.version}.min.js`,
+      format: 'esm',
+      plugins: [terser()]
+    },
+    {
+      file: `dist/sketchlib.min.js`,
+      format: 'esm',
+      plugins: [terser()]
+    }
+  ],
   plugins: [
     resolve(),
     commonjs()
